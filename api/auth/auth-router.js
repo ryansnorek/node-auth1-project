37656fa9acf5993bb4865eabe194e3b96f604604
Ryan <snorekr@gmail.com>
2021-12-14 09:26:38 -0500

    retry

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,11 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
-
-
+const router = require("express").Router();
+const {
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+} = require("./auth-middleware");
+const User = require("../users/users-model");
+const bcrypt = require("bcryptjs");
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
@@ -24,7 +28,21 @@
     "message": "Password must be longer than 3 chars"
   }
  */
+router.post(
+  "/register",
+  checkPasswordLength,
+  checkUsernameFree,
+  (req, res, next) => {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
 
+    User.add({ username, password: hash })
+      .then((savedUser) => {
+        res.status(201).json(savedUser);
+      })
+      .catch(next);
+  }
+);
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +59,15 @@
     "message": "Invalid credentials"
   }
  */
-
+router.post("/login", checkUsernameExists, (req, res, next) => {
+  const { password } = req.body
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    next({ message: `Welcome ${req.body.username}` })
+  } else {
+    next({ status: 401, message: "Invalid credentials" })
+  }
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +85,13 @@
   }
  */
 
- 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy()
+    res.json({ message: "logged out" })
+  } else {
+    res.json({ status: 200, message: "no session" })
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
+const usersRouter = require("./users/users-router")
+const authRouter = require("./auth/auth-router")
+const session = require("express-session")
+const Store = require("connect-session-knex")(session)
+const knex = require("../data/db-config")
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -15,11 +19,35 @@ const cors = require("cors");
   or you can use a session store like `connect-session-knex`.
  */
 
+
+
 const server = express();
 
+server.use(session({
+  name: "chocolatechip",
+  secret: "x",
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    tablename: "sessions",
+    sidfieldname: "sid"
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+    // sameSite: "none" // enable 3rd party cookies
+  }
+}))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/users", usersRouter)
+server.use("/api/auth", authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
